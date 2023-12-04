84ad9329706d9f444511b046821e2d50e5c32863
simidot <114278754+simidot@users.noreply.github.com>
2023-12-04 19:21:17 +0900

    [Bronze II] Title: OX퀴즈, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// OX 퀴즈
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 개수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cases = Integer.parseInt(br.readLine());
+        String[] inputs = new String[cases];
+
+        for (int i=0; i<cases; i++) {
+            inputs[i] = br.readLine();
+        }
+
+        int plusScore = 0; // 얼마나 더할지
+        int sum = 0; // 전체 합산
+
+        for (int i=0; i<cases; i++) {
+            for(int j=0; j<inputs[i].length(); j++) {
+                char ox = inputs[i].charAt(j);
+
+                if (ox=='O') {
+                    plusScore++;
+                    sum += plusScore;
+                } else {
+                    plusScore = 0;
+                }
+            }
+            System.out.println(sum);
+            plusScore=0;
+            sum = 0;
+        }
+    }
+}
