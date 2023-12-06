407d0d8352d03af7a8521697ba2918ddaf137e25
simidot <114278754+simidot@users.noreply.github.com>
2023-12-06 15:28:09 +0900

    [level unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -6,19 +6,15 @@ class Solution {
             case "<":
                 if(eq.equals("=")) {
                     if (n<=m) answer = 1;
-                    System.out.println("1 :"+answer);
                 } else {
                     if (n<m) answer=1;
-                    System.out.println("2 :"+answer);
                 }
                 break;
             case ">":
                 if (eq.equals("=")) {
                     if (n>=m) answer = 1;
-                    System.out.println("3 :"+answer);
                 } else {
                     if (n>m) answer = 1;
-                    System.out.println("4: "+answer);
                 }
                 break;
         }
