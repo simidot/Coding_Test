8b08deeeeca92c9928a6eac4435f0967d6569423
simidot <114278754+simidot@users.noreply.github.com>
2023-12-07 23:54:47 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.10 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = arr;
+
+        for (int i=0; i<query.length; i++) {
+            if (i%2==0) { // 짝수 인덱스에서는 arr에서 query[i]번 인덱스 뒷부분 제거
+                arr = Arrays.copyOfRange(arr, 0, query[i]+1);
+            } else {// 홀수 인덱스에서는 arr에서 query[i]번 인덱스 앞부분 제거
+                arr =Arrays.copyOfRange(arr, query[i], arr.length);
+            }
+        }
+        return arr;
+    }
+}
