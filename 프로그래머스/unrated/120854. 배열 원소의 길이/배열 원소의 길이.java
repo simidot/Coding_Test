bbdcf1a78ca2e4bc4caf052b91c3bc8890cf7870
simidot <114278754+simidot@users.noreply.github.com>
2023-12-12 16:08:22 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 2.83 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,11 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
 
-        // strlist의 각 원소의 길이를 담은 배열
-        for(int i=0; i<strlist.length; i++) {
-            answer[i] = strlist[i].length();
-        }
+        answer = Arrays.stream(strlist).mapToInt(String::length).toArray();
+
 
         return answer;
     }
