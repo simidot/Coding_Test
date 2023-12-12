2e8d4351b4c55b9bc149371c8518f2deba6ff03c
simidot <114278754+simidot@users.noreply.github.com>
2023-12-12 17:24:21 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 132 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -1,17 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 // 엄청난 부자2
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 돈을 똑같이 분배 + 동일하게 분배하고 남은 돈?
         // 조교가 가진 돈 n / 돈을 받으러 온 생명체의 수 m
-        Scanner sc = new Scanner(System.in);
-        
-        BigInteger n = sc.nextBigInteger();
-        BigInteger m = sc.nextBigInteger();
-        sc.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] brArray = br.readLine().split(" ");
+        BigInteger n = new BigInteger(brArray[0]);
+        BigInteger m = new BigInteger(brArray[1]);
 
         System.out.println(n.divide(m));
         System.out.println(n.remainder(m));
