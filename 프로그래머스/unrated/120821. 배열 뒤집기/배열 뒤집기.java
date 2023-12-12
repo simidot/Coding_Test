f55cdb2feed638c8e1a1d3b862d88d2eda06136b
simidot <114278754+simidot@users.noreply.github.com>
2023-12-12 14:48:56 +0900

    [level unrated] Title: 배열 뒤집기, Time: 4.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
+        List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
 
-        for (int i=0; i<num_list.length; i++) {
-            answer[num_list.length-i-1] = num_list[i];
-        }
-
-        return answer;
+        Collections.reverse(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
