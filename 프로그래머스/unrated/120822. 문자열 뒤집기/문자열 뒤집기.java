dd102d7e7f0d63016934735aebcf212f5e98d1f7
simidot <114278754+simidot@users.noreply.github.com>
2023-12-12 15:27:17 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,10 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        char[] string = my_string.toCharArray();
         StringBuffer sb = new StringBuffer();
+        sb.append(my_string);
+        sb.reverse();
 
-        for(int i=string.length-1; i>=0; i--) {
-            sb.append(string[i]);
-        }
         answer = sb.toString();
         return answer;
     }
