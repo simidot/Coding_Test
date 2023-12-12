ea030ae43ecf2686b11deb47a167723a852fea94
simidot <114278754+simidot@users.noreply.github.com>
2023-12-12 15:27:49 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.11 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
+        
         StringBuffer sb = new StringBuffer();
         sb.append(my_string);
         sb.reverse();
-
         answer = sb.toString();
         return answer;
     }
