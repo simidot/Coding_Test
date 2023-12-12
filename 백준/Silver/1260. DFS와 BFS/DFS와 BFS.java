2d8db91ba6bed4d4bf491723133adb5a241143bc
simidot <114278754+simidot@users.noreply.github.com>
2023-12-12 19:34:35 +0900

    [Silver II] Title: DFS와 BFS, Time: 260 ms, Memory: 23652 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -5,99 +5,73 @@ import java.util.*;
 
 // DFS와 BFS
 public class Main {
-    public static int[][] map;
-    public static int N;
-    public static int V;
+    static int N;
+    static int M;
+    static int V;
+    static StringBuffer sb;
+    static boolean[] visited;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 첫째 줄 정점의 개수 N, 간선의 개수 M, 탐색 시작할 번호 V
-        String[] inputs = br.readLine().split(" ");
-        N = Integer.parseInt(inputs[0]); //방문지점 개수
-        int M = Integer.parseInt(inputs[1]); //연결다리 개수
-        V = Integer.parseInt(inputs[2]); // 시작번호
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        map = new int[N + 1][N + 1]; //방문지점과 각 지점의 연결성을 표시하는 맵
+        N= Integer.parseInt(st.nextToken());
+        M= Integer.parseInt(st.nextToken());
+        V= Integer.parseInt(st.nextToken());
 
-        // 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호.
-        for (int i = 0; i < M; i++) {
-            String[] connects = br.readLine().split(" ");
+        map = new int[N+1][N+1];
+        visited = new boolean[N+1]; //visited 초기화
+        sb = new StringBuffer();
 
-            int left = Integer.parseInt(connects[0]);
-            int right = Integer.parseInt(connects[1]);
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            map[left][right] = 1;
-            map[right][left] = 1;
-        } // 맵 완성
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
 
+            map[left][right]=1;
+            map[right][left]=1;
+        }
 
-        System.out.println(dfs());
-        System.out.println(bfs());
+        dfs(V);
+        System.out.println(sb);
+        visited = new boolean[N+1]; //visited 초기화
+        sb.delete(0, sb.length()); //stringBuffer 초기화
+        bfs(V);
+        System.out.println(sb);
 
     }
 
-    public static String dfs() {
-        // 첫째줄에는 DFS 결과
-        // 1. Stack
-        // 2. List 방문한 곳 저장
-        // 3. 방문 여부 저장 boolean
-        Stack<Integer> toVisit = new Stack<>();
-        List<Integer> visitOrder = new ArrayList<>();
-        boolean[] visited = new boolean[N+1];
+    public static void dfs(int V){
+        visited[V]=true; //들어온 순간 true가 됨.
+        sb.append(V+" "); //그 값을 sb에 덧붙임
 
-        toVisit.push(V); //첫 시작이니 스택에 넣고 시작
-
-        while (!toVisit.empty()) {
-            int num = toVisit.pop(); // 꺼낸 것이 확인할 num
-
-            if (visited[num]) continue;
-
-            visitOrder.add(num);
-            visited[num] = true;
-
-            for (int i = N; i > 0; i--) {
-                if(visited[i]) continue;
-                if (map[num][i] == 1) {
-                    toVisit.push(i);
-                }
+        for(int i=1; i<=N; i++) { //V와 연결된 것을 찾는다
+            if(map[V][i]==1&&!visited[i]){
+                dfs(i);
             }
         }
-        StringBuffer sb = new StringBuffer();
-        for(int i:visitOrder){
-            sb.append(i+" ");
-        }
-        return sb.toString();
     }
 
+    public static void bfs(int V){
+        Queue<Integer> myQueue = new LinkedList<>();
 
-    public static String bfs() {
-        // 둘째줄에는 BFS 결과
-        Queue<Integer> toVisit2 = new LinkedList<>();
-        List<Integer> visitOrder2 = new ArrayList<>();
-        boolean[] visited = new boolean[N+1];
+        myQueue.offer(V);
 
+        while(!myQueue.isEmpty()){
+            int next = myQueue.poll();
 
-        toVisit2.offer(V);
+            if(visited[next]) continue;
+            sb.append(next+" ");
+            visited[next] = true;
 
-        while(!toVisit2.isEmpty()) {
-            int num2 = toVisit2.poll();
-
-            if(visited[num2]==true) continue;
-
-            visitOrder2.add(num2);
-            visited[num2] = true;
-
-            for(int i=1; i<N+1; i++) {
+            for(int i=1; i<=N; i++) {
                 if(visited[i]) continue;
-                if(map[num2][i]==1) {
-                    toVisit2.offer(i);
+                if (map[next][i] == 1) {
+                    myQueue.offer(i);
                 }
             }
         }
-        StringBuffer sb = new StringBuffer();
-        for(int i:visitOrder2){
-            sb.append(i+" ");
-        }
-        return sb.toString();
     }
 }
