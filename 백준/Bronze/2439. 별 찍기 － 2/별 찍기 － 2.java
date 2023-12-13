38b04fbc099cf12702a64c99f9804d9cc78e4a72
simidot <114278754+simidot@users.noreply.github.com>
2023-12-13 15:18:47 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 276 ms, Memory: 15840 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 별 찍기-2 브론즈4
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=N; i++) {
+            for(int j=N-i; j>0; j--){
+                System.out.print(" ");
+            }
+            for(int k=0; k<i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
