297f8564d15f37a1df58eabbacb79ccdebad8080
simidot <114278754+simidot@users.noreply.github.com>
2023-12-13 17:01:34 +0900

    [Bronze I] Title: 단어 공부, Time: 324 ms, Memory: 23416 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//단어 공부 브론즈1
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toUpperCase().toCharArray();
+        Arrays.sort(input); //일단 정렬...?
+
+        int[] count = new int[91];
+        // 가장 많이 사용된 알파벳을 대문자로 출력.
+        // 아스키코드로 표현하면 A는 65 Z는 90
+        for(int i=0; i<input.length; i++) {
+            count[input[i]]++;
+        }
+        // 여러개면 ? 출력
+        int max = count[0];
+        char answer = ' ';
+        for(int i=1; i<count.length; i++) {
+            if(count[i] > max) {
+                max = count[i];
+                answer = (char)i;
+            } else if (count[i]==max) {
+                answer = '?';
+            }
+        }
+        System.out.println(answer);
+    }
+}
