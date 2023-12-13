02c0cfdf7754907d1fd006d38840a7037c3fd7f6
simidot <114278754+simidot@users.noreply.github.com>
2023-12-13 14:55:07 +0900

    [Bronze V] Title: 나머지, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//나머지 브론즈5
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
