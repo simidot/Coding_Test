75be4baf29798af5aa88c815393f501bfc1032e4
simidot <114278754+simidot@users.noreply.github.com>
2023-12-15 11:08:46 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while(true) {
-            String[] inputs = sc.nextLine().split(" ");
+            String[] inputs = br.readLine().split(" ");
             if(inputs[0].equals("0")) break;
             int a = Integer.parseInt(inputs[0]);
             int b = Integer.parseInt(inputs[1]);
