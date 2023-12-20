8e57f8ffbf1abf528358f3e07e83e47707ae7922
simidot <114278754+simidot@users.noreply.github.com>
2023-12-21 08:55:36 +0900

    [level unrated] Title: 배열 자르기, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -1,18 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2-num1+1];
-
-        List<Integer> array = new ArrayList<>();
-        //정수배열의 num1번째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 출력
-        for(int i=num1; i<=num2; i++) {
-            array.add(numbers[i]);
-        }
-
-        for(int i=0; i<array.size(); i++) {
-            answer[i] = array.get(i);
-        }
+        int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
         return answer;
     }
 }
