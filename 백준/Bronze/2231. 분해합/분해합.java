bd33d9a7c67e77263269ec3c5eb0351c509f7102
simidot <114278754+simidot@users.noreply.github.com>
2023-12-20 11:39:34 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -8,20 +8,26 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        String input = br.readLine();
         int answer = 0;
-        for(int i=1; i<N; i++) {
+
+        // 입력 받은 정수에서 자릿수길이만큼 9를 빼준다.
+        //그보다 작다면? 생성자가 될 수 없다!!
+        // 탐색을 거기부터 시작하면 된다.
+        
+        // 자릿수길이 알기 위해 
+        int N_length =input.length();
+        int N = Integer.parseInt(input);
+        
+        for(int i=N-(9*N_length); i<N; i++) {
             int M=i;
-            int sum = M;
-            List<Integer> numList = new ArrayList<>();
+            int sum = 0;
             while(M%10!=0) {
-                numList.add(M%10);
-                M /= 10;
+                sum +=(M%10); //각 자릿수 더하기
+                M /= 10; // 더한 후 10으로 나누어 마지막자리 제거
             }
-            for(int j: numList) {
-                sum= sum+j;
-            }
-            if(sum == N) {
+
+            if(sum +i == N) {
                 answer = i;
                 break;
             }
