e42f32e501d9a287e8b205d066b750a37669fbe4
simidot <114278754+simidot@users.noreply.github.com>
2023-12-21 11:46:21 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.14 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -3,15 +3,14 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
-        // 환자의 응급도 기준으로 진료순서
-
-        int[] newArray = Arrays.stream(emergency).sorted().toArray();
-        for (int i=0; i<emergency.length; i++) {
-            for(int j=0; j<newArray.length; j++) {
-                if (emergency[i]==newArray[j]) {
-                    answer[i] = newArray.length-j;
+        for(int i=0; i<emergency.length; i++) {
+            int idx = 1;
+            for(int j=0; j<answer.length; j++) {
+                if (emergency[i] < emergency[j]) {
+                    idx++;
                 }
             }
+            answer[i] = idx;
         }
         System.out.println(Arrays.toString(answer));
         return answer;
