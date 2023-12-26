6a30a9e3fb05eb5ae9faf8fb284d5d4e8b3e44b8
simidot <114278754+simidot@users.noreply.github.com>
2023-12-26 16:28:32 +0900

    [Silver IV] Title: 스택, Time: 392 ms, Memory: 20696 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//스택 실버4
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<N;i++) {
+            String[] inputs = br.readLine().split(" ");
+            if(inputs.length >1) {
+                num = Integer.parseInt(inputs[1]);
+            }
+
+            switch (inputs[0]) {
+                case "push":
+                    stack.push(num);
+                    break;
+                case "pop":
+                    if(stack.isEmpty()) {
+                        System.out.println("-1");
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println("1");
+                    } else {
+                        System.out.println("0");
+                    }
+                    break;
+                case "top":
+                    if(stack.isEmpty()) {
+                        System.out.println("-1");
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+
+
+        //명령 처리 프로그램
+        // push X : 스택에 X 넣기
+        // pop : 스택에서 pop, 없는 경우 -1
+        // size : 스택에 들어있는 정수의 개수 출력
+
+    }
+}
