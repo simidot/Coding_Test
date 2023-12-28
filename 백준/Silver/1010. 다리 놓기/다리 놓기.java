3b98bbaf118c8f10b01e0147d7924304ccb76fbd
simidot <114278754+simidot@users.noreply.github.com>
2023-12-28 20:39:02 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -5,34 +5,31 @@ import java.util.StringTokenizer;
 
 //다리 놓기 실버5
 public class Main {
-    private static final int[][] dp = new int[31][31];
-
     public static void main(String[] args) throws IOException {
-        //테스트 케이스의 수 T
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuffer sb = new StringBuffer();
-        // 각각의 테스트케이스에 대해 강의 서쪽과 동쪽에 있는 사이트의 개수 정수 N, M
+
+        int[][] dp = new int[30][30];
+
+        for(int i=1; i<30; i++) { // n==r일 때와 r==0일때 1로 설정
+            dp[i][i] = 1; //n==r
+            dp[i][0] = 1; //r==0
+        }
+
+        // 그 외에는 nCr = n-1Cr-1 + n-1Cr 규칙 사용하여 계산
+        for(int i=2; i<30; i++) {
+            for(int j=1; j<30; j++) {
+                dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+            }
+        }
+
         for(int i=0; i<T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            // N개만큼 다리를 지으려고 할 때 다리를 지을 수 있는 경우의 수를 구한다.
-            // mPn
-            sb.append(combination(M, N)).append("\n");
+            sb.append(dp[M][N]).append("\n");
         }
         System.out.println(sb);
-        br.close();
-    }
-    public static int combination(int n, int r){
-        //이미 계산된 값일 경우
-        if (dp[n][r]>0) {
-            return dp[n][r];
-        }
-        else if(n==r||r==0) { //원소의 개수가 조합의 갯수와 동일하거나 0일 경우
-            return dp[n][r] =1;
-        } else {
-            return dp[n][r] = combination(n - 1, r - 1) + combination(n - 1, r);
-        }
     }
 }
