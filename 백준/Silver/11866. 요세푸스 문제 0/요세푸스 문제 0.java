f8698c14b15d185e4a27cbb92a1fc0750f85b51f
simidot <114278754+simidot@users.noreply.github.com>
2024-01-24 16:23:40 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 160 ms, Memory: 22864 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -9,16 +9,11 @@ import java.util.StringTokenizer;
 //요세푸스 문제 0 실버5
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(<=N)
-        // 순서대로 K번째 사람을 제거한다... > N명의 사람이 제거될때까지 계속.
-        // 제거되는 순서를 요세푸스 순열.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        // 큐를 활용한다.
-        // 1,2번째는 뽑고poll 맨뒤로 옮기고offer, 3번째는 뽑기만하기.
         Queue<Integer> queue = new LinkedList<>();
         StringBuffer sb = new StringBuffer();
         sb.append("<");
@@ -27,7 +22,7 @@ public class Main {
             queue.add(i);
         }
 
-        while (!queue.isEmpty()) {
+        while (queue.size()>1) {
             for(int i=1; i<=N; i++) {
                 if (i == K) {
                     sb.append(queue.poll()).append(", ");
@@ -37,9 +32,7 @@ public class Main {
                 }
             }
         }
-
-        sb.delete(sb.length()-2,sb.length());
-        sb.append(">");
+        sb.append(queue.poll()).append(">");
         System.out.println(sb);
     }
 }
