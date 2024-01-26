3b93c38f89534d832554dd3f9dfbf9206e618b8b
simidot <114278754+simidot@users.noreply.github.com>
2024-01-26 21:06:44 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,22 +2,8 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
 
-        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
-
-        StringBuffer sb = new StringBuffer();
-        char[] chars = my_string.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            for (int j = 0; j < vowels.length; j++) {
-                if (chars[i] == vowels[j]) {
-                    chars[i] = 0;
-                    break;
-                }
-            }
-            if (chars[i] != 0) {
-                sb.append(chars[i]);
-            }
-        }
-        answer = sb.toString();
+        answer = my_string.replaceAll("[aeiou]", "");
+        
         return answer;
     }
 }
