df6d7f678a0ccb60533586f541faf9be6b988227
simidot <114278754+simidot@users.noreply.github.com>
2024-10-14 15:56:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.99 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -7,16 +7,20 @@ class Solution {
         // participant = 마라톤 참여 선수들의 이름
         // completion = 완주한 선수들의 이름
         // 단 한 명 완주하지 못한 선수들의 이름을 return
-        // 동명이인이 있음.
+        // 동명이인이 있다.
 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        answer = participant[participant.length-1];
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                answer = participant[i];
-                break;
+  HashMap<String, Integer> result = new HashMap<>();
+
+        for (String player : participant) {
+            result.put(player, result.getOrDefault(player, 0) + 1);
+        }
+        for (String player : completion) {
+            result.put(player, result.get(player) -1);
+        }
+
+        for (String key : result.keySet()) {
+            if (result.get(key) != 0) {
+                answer = key;
             }
         }
         return answer;
