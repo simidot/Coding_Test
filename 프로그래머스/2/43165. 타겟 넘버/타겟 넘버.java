105804ef1e4272dc54b2c3ba19eba8421944869a
simidot <114278754+simidot@users.noreply.github.com>
2024-10-15 13:12:45 +0900

    [level 2] Title: 타겟 넘버, Time: 5.23 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
-        
-        // 음이 아닌 정수들
-        // 순서를 바꾸지 않고 더하거나 빼서 타겟 넘버를 만들려고 한다.
-        // 타겟 넘버를 만드는 방법의 수
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        for (int i = 0; i < numbers.length; i++) {
-            int size = queue.size();
-            for (int j = 0; j < size; j++) {
-                int sum = queue.poll();
-                queue.add(sum + numbers[i]);
-                queue.add(sum - numbers[i]);
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            if (queue.poll() == target) {
-                answer ++;
-            }
-        }
-        
-        return answer;
-    }
+    int answer = 0;  
+  
+    public int solution(int[] numbers, int target) {  
+        dfs(0, numbers, target, 0);  
+        return answer;  
+    }  
+  
+    private void dfs(int depth, int[] numbers, int target, int sum) {  
+        if (depth == numbers.length) {  
+            if (sum == target) {  
+                answer++;  
+            }  
+            return;  
+        }  
+        dfs(depth + 1, numbers, target, sum + numbers[depth]);  
+        dfs(depth + 1, numbers, target, sum - numbers[depth]);  
+    }  
 }
