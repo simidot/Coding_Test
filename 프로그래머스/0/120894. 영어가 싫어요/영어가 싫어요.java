5f06417d16a71d91a1273f33826b38221a94f172
simidot <114278754+simidot@users.noreply.github.com>
2024-12-17 22:54:18 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.69 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -2,20 +2,13 @@ class Solution {
     public long solution(String numbers) {
         long answer = 0;
         
-        numbers = numbers.replace("zero", "0");
-        numbers = numbers.replace("one", "1");
-        numbers = numbers.replace("two", "2");
-        numbers = numbers.replace("three", "3");
-        numbers = numbers.replace("four", "4");
-        numbers = numbers.replace("five", "5");
-        numbers = numbers.replace("six", "6");
-        numbers = numbers.replace("seven", "7");
-        numbers = numbers.replace("eight", "8");
-        numbers = numbers.replace("nine", "9");
+        String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        answer = Long.parseLong(numbers);
+        for (int i=0; i<numbers_arr.length; i++) {
+            numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
+        }
 
-        
+        answer = Long.parseLong(numbers);
         return answer;
     }
 }
