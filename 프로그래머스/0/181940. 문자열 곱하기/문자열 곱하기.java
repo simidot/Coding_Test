f44c205594ca337db342ddedb8e63630e1d8da29
simidot <114278754+simidot@users.noreply.github.com>
2024-12-17 19:18:36 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String my_string, int k) {
-        String answer = "";
-            StringBuilder sb = new StringBuilder();
-            for (int i=0; i<k; i++) {
-                sb.append(my_string);
-            }
-            answer = sb.toString();
-            return answer;
+        String answer = my_string.repeat(k);
+        return answer;
     }
 }
