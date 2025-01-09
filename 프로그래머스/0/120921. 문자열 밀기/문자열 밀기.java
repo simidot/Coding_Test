90a15562b6417972157a308e7bf4c79a2b0d26e6
simidot <114278754+simidot@users.noreply.github.com>
2025-01-09 17:45:04 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        
-        String temp = A;
-        for (int i=0; i<A.length(); i++) {
-            if (temp.equals(B)) {
-                return answer;
-            } 
-            String last = temp.substring(temp.length()-1);
-            temp = last + temp.substring(0, temp.length()-1);
-            answer++;
-        } 
-        
-        return -1;
+        String tempB = B.repeat(3);
+        return tempB.indexOf(A);
     }
 }
