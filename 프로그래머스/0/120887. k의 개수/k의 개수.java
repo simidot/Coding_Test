4189101b33fb6fe0935b93859d3d98874f41f224
simidot <114278754+simidot@users.noreply.github.com>
2025-01-17 15:44:53 +0900

    [level 0] Title: k의 개수, Time: 65.26 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        
+        String strK = String.valueOf(k);
+        for (int l=i; l<=j; l++) {
+            String strL = String.valueOf(l);
+            if (strL.contains(strK)) {
+                String[] array = strL.split("");
+                for (String a: array) {
+                    if (a.equals(strK)) answer++;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
